8b70c67ac0000a9b7b76272348d493ed1ebb3413
unknown <hever11.hdgd@gmail.com>
2022-07-26 12:27:09 -0500

    Styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import './App.css';
+import Logo from '../src/assets/logo.png';
 import CharactersQuery from './querys';
 
 function App() {
   return (
     <div className="App">
+      <figure>
+        <img src={Logo} alt="logo" />
+      </figure>
     <CharactersQuery />
     </div>
   );
