69a5e6b338e9fc756d9867eee7f564e7d11b8392
unknown <hever11.hdgd@gmail.com>
2022-07-26 11:52:59 -0500

    fix

--- src/querys.js
+++ src/querys.js
@@ -5,22 +5,27 @@ import { useQuery, gql } from '@apollo/client';
 const GET_CHARACTERS = gql`
     query GetCharacters {
         characters {
-            id
-            name
-            image
+            results {
+                id
+                name
+                image
+            }
         }
-
     }
-`;
  
-export default function CharactersQuery() {
+`;
+
+ 
+function CharactersQuery() {
     const { loading, error, data } = useQuery(GET_CHARACTERS);
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error no data :( </p>;
-    return data.characters.map(({id, name, image}) => (
+    return data.characters.results.map(({id, name, image})  => (
         <div key={id}>
             <img src={image} alt={name} />
             <h3>{name}</h3>
         </div>
     ));
 }
+
+export default CharactersQuery;
