bbe4ca344194380dbb86d1d9805251bac4c057de
unknown <hever11.hdgd@gmail.com>
2022-07-28 21:04:48 -0500

    Updates

---
+++ precache-manifest.1884716888905947b52dd2f102efe3f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07ba3decb2d79f826f59",
+    "url": "/rickandmortyGRAPHQL/static/css/main.92641f68.chunk.css"
+  },
+  {
+    "revision": "07ba3decb2d79f826f59",
+    "url": "/rickandmortyGRAPHQL/static/js/main.07ba3dec.chunk.js"
+  },
+  {
+    "revision": "999c7ccae0859f68a475",
+    "url": "/rickandmortyGRAPHQL/static/js/1.999c7cca.chunk.js"
+  },
+  {
+    "revision": "e210c5375d0463e01cfd",
+    "url": "/rickandmortyGRAPHQL/static/js/runtime~main.e210c537.js"
+  },
+  {
+    "revision": "9678650c9c889d6937ae5e0e90583c84",
+    "url": "/rickandmortyGRAPHQL/static/media/logo.9678650c.png"
+  },
+  {
+    "revision": "d83010dafa73058f9e5889a304ced531",
+    "url": "/rickandmortyGRAPHQL/index.html"
+  }
+];
